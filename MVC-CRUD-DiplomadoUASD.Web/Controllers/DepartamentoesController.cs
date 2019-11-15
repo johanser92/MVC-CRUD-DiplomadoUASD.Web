7c5e0b68a8fd49397151d251288668d55723673a
johanser92 <40702293+johanser92@users.noreply.github.com>
2019-11-15 17:02:32 -0400

    Proyecto 6-2

---
+++ MVC-CRUD-DiplomadoUASD.Web/Controllers/DepartamentoesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC_CRUD_DiplomadoUASD.Models.Models;
+
+namespace MVC_CRUD_DiplomadoUASD.Web.Controllers
+{
+    public class DepartamentoesController : Controller
+    {
+        private EmpleadoDBEntities db = new EmpleadoDBEntities();
+
+        // GET: Departamentoes
+        public ActionResult Index()
+        {
+            var departamentoes = db.Departamentoes.Include(d => d.Empleado);
+            return View(departamentoes.ToList());
+        }
+
+        // GET: Departamentoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Departamento departamento = db.Departamentoes.Find(id);
+            if (departamento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(departamento);
+        }
+
+        // GET: Departamentoes/Create
+        public ActionResult Create()
+        {
+            ViewBag.Id_Dep = new SelectList(db.Empleadoes, "ID", "Nombre");
+            return View();
+        }
+
+        // POST: Departamentoes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id_Dep,Nombre")] Departamento departamento)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Departamentoes.Add(departamento);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Id_Dep = new SelectList(db.Empleadoes, "ID", "Nombre", departamento.Id_Dep);
+            return View(departamento);
+        }
+
+        // GET: Departamentoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Departamento departamento = db.Departamentoes.Find(id);
+            if (departamento == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Id_Dep = new SelectList(db.Empleadoes, "ID", "Nombre", departamento.Id_Dep);
+            return View(departamento);
+        }
+
+        // POST: Departamentoes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id_Dep,Nombre")] Departamento departamento)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(departamento).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Id_Dep = new SelectList(db.Empleadoes, "ID", "Nombre", departamento.Id_Dep);
+            return View(departamento);
+        }
+
+        // GET: Departamentoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Departamento departamento = db.Departamentoes.Find(id);
+            if (departamento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(departamento);
+        }
+
+        // POST: Departamentoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Departamento departamento = db.Departamentoes.Find(id);
+            db.Departamentoes.Remove(departamento);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
