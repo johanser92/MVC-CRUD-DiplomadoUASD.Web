4fcd85517ac8390cc7ef082fd9ca449e5df713be
johanser92 <40702293+johanser92@users.noreply.github.com>
2019-11-18 15:25:38 -0400

    Johanser
    Actualizacion de codigo

---
+++ MVC-CRUD-DiplomadoUASDCodeFirst.Model/DAL/EmpleadoContext.cs
@@ -0,0 +1,30 @@
+﻿using MVC_CRUD_DiplomadoUASDCodeFirst.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_CRUD_DiplomadoUASDCodeFirst.Model.DAL
+{
+    public class EmpleadoContext : DbContext
+       
+    {
+        public EmpleadoContext()
+             : base("EmpleadoContext")
+        { 
+        }
+
+         public DbSet<Empleado> Empleados { get; set; }
+         public DbSet<Registro> Registros { get; set; }
+         public DbSet<Departamento> Departamentos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingEntitySetNameConvention>();
+        }
+
+    }
+}
---
+++ MVC-CRUD-DiplomadoUASDCodeFirst.Model/Models/Departamento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_CRUD_DiplomadoUASDCodeFirst.Model.Models
+{
+   public class Departamento
+    {
+        public int DepartamentoID { get; set; }
+        public string Descripcion { get; set; }
+        public virtual ICollection<Registro> Registros { get; set; }
+    }
+}
